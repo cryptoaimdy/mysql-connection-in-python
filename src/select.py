991e5f697732110e91cad0ce8c19485701d0946d
MOHAMMAD ALI <48402360+cryptoaimdy@users.noreply.github.com>
2019-08-17 16:22:02 +0530

    Update select.py

--- src/select.py
+++ src/select.py
@@ -11,7 +11,7 @@ try:
    connection = mysql.connector.connect(host='localhost',
                              database='Cryptocurrencies',
                              user='root',
-                             password='natural786')
+                             password='password')
    sql_select_query = "SELECT * from Cryptocoins"
 
    cursor = connection.cursor()
